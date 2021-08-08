aec2c0c068eb20ad17cec7646c8c3d41fdc5467d
Ethin Probst <ethindp@protonmail.com>
2021-08-07 22:52:14 -0500

    Begin refactoring of HdaAudio application.
    Signed-off-by: Ethin Probst <ethindp@protonmail.com>

--- MdeModulePkg/Application/HdaAudio/HdaAudio.h
+++ MdeModulePkg/Application/HdaAudio/HdaAudio.h
@@ -45,43 +45,31 @@ typedef enum {
   HdaImmCmdStatus = 0x68,
   HdaDplLo = 0x70,
   HdaDplHi = 0x74,
-  HdaRegMax = 0xFFFFFFFFFFFFFFFF // Force to UINT64
+  HdaStreamControl = 0x80,
+  HdaStreamStatus = 0x83,
+  HdaStreamLinkPosInBuf = 0x84,
+  HdaStreamCyclicBufferLength = 0x88,
+  HdaStreamLastValidIndex = 0x8C,
+  HdaStreamFifoSize = 0x90,
+  HdaStreamFormat = 0x92,
+  HdaStreamBdlLo = 0x98,
+  HdaStreamBdlHi = 0x9C,
+  HdaRegMax = MAX_UINT64
 } HDA_REGISTER;
 
 #pragma pack(push)
 #pragma pack(1)
 typedef union {
   struct {
-    ///
-    /// If 1, this controller supports 64-bit addresses. If 0, only 32-bit addresses are supported
-    ///
     UINT16 Supports64BitAddresses:1;
-    ///
-    /// The number of serial data output (SDO) lines
-    /// supported by the controller. There may be
-    /// up to 4 SDOs supported.
-    ///
     UINT16 SerialDataOutputLines:2;
-    ///
-    /// The number of bidirectional streams that are supported
-    /// by this controller. There may be up to 30 streams supported.
-    ///
     UINT16 BidirectionalStreams:5;
-    ///
-    /// The number of input streams supported
-    /// by this controller. There may be up to 15 input streams supported.
-    ///
     UINT16 InputStreams:4;
-    ///
-    /// The number of output streams supported
-    /// by this controller. There may be up to 15 output streams supported.
-    ///
   UINT16 OutputStreams:4;
   } Bits;
   UINT16 Raw;
 } HDA_GLOBAL_CAPABILITIES;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT32 ControllerReset:1;
@@ -93,7 +81,6 @@ typedef union {
   UINT32 Raw;
 } HDA_GLOBAL_CONTROL;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT16 rsvd1:1;
@@ -103,7 +90,6 @@ typedef union {
   UINT16 Raw;
 } HDA_GLOBAL_STATUS;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT32 interrupts:30;
@@ -113,7 +99,6 @@ typedef union {
   UINT32 Raw;
 } HDA_INTERRUPT_STATUS;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT32 StreamInterrupts:30;
@@ -123,7 +108,6 @@ typedef union {
   UINT32 Raw;
 } HDA_INTERRUPT_CONTROL;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT16 ReadPtr:8;
@@ -133,7 +117,6 @@ typedef union {
   UINT16 Raw;
 } HDA_CORB_RDPTR;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT8 MemErrIntEnable:1;
@@ -143,7 +126,6 @@ typedef union {
   UINT8 raw;
 } HDA_CORB_CONTROL;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT8 MemErr:1;
@@ -152,7 +134,6 @@ typedef union {
 UINT8 Raw;
 } HDA_CORB_STATUS;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT16 WritePtr:8;
@@ -161,7 +142,6 @@ typedef union {
   UINT16 Raw;
 } HDA_CORB_WRPTR;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT8 Size:2;
@@ -171,7 +151,6 @@ typedef union {
   UINT8 Raw;
 } HDA_CORB_SIZE;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT16 WritePtr:8;
@@ -181,7 +160,6 @@ typedef union {
   UINT16 Raw;
 } HDA_RIRB_WRPTR;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT16 Count:8;
@@ -190,7 +168,6 @@ typedef union {
   UINT16 Raw;
 } HDA_RESP_INT_CNT;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT8 ResponseInterrupt:1;
@@ -201,7 +178,6 @@ typedef union {
   UINT8 Raw;
 } HDA_RIRB_CONTROL;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT8 response:1;
@@ -212,7 +188,6 @@ typedef union {
   UINT8 Raw;
 } HDA_RIRB_STATUS;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT8 Size:2;
@@ -222,13 +197,11 @@ typedef union {
   UINT8 Raw;
 } HDA_RIRB_SIZE;
 
-#pragma pack(1)
 typedef struct {
   UINT32 Position;
   UINT32 rsvd;
 } HDA_STREAM_DMA_POSITION;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT32 InterruptOnCompletion:1;
@@ -237,14 +210,12 @@ typedef union {
   UINT32 Raw;
 } HDA_BDLE_CONFIG;
 
-#pragma pack(1)
 typedef struct {
   UINT64 Address;
   UINT32 Length;
   HDA_BDLE_CONFIG Config;
 } HDA_BUFFER_DESCRIPTOR_LIST_ENTRY;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT32 Data:8;
@@ -255,7 +226,6 @@ typedef union {
   UINT32 Raw;
 } HDA_CORB_ENTRY;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT32 SdoLine:4;
@@ -265,7 +235,6 @@ typedef union {
   UINT32 Raw;
 } HDA_RIRB_RESP_EX;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT32 Response:21;
@@ -275,13 +244,11 @@ typedef union {
   UINT32 SolicitedResponse;
 } HDA_RIRB_RESPONSE_CONTENTS;
 
-#pragma pack(1)
 typedef struct {
   HDA_RIRB_RESPONSE_CONTENTS Contents;
   HDA_RIRB_RESP_EX Info;
 } HDA_RIRB_RESPONSE;
 
-#pragma pack(1)
 typedef union {
   struct {
     UINT16 Channels:4;
@@ -296,4 +263,86 @@ typedef union {
 } HDA_STREAM_FORMAT_DESCRIPTOR;
 
 #pragma pack(pop)
+
+typedef struct {
+  /// Handle to this HDA controller if required
+  EFI_HANDLE Handle;
+  /// The pointer to the allocated EFI_PCI_IO_PROTOCOL protocol instance for this device
+  PciIo* PciIo;
+  /// Size of the CORB in bytes
+  UINTN CorbBytes;
+  /// Size of the RIRB in bytes
+  UINTN RirbBytes;
+  /// The total number of CORB entries that can be written to the CORB
+  UINT8 MaxCorbEntries;
+  /// The total number of RIRB entries that can be read from the RIRB
+  UINT8 MaxRirbEntries;
+  /// Implementation-defined mapping handle for the CORB
+  VOID* CorbMapping;
+  /// Implementation-defined mapping handle for the RIRB
+  VOID* RirbMapping;
+  /// Physical address to the CORB
+  EFI_PHYSICAL_ADDRESS CorbPhysAddress;
+  /// Physical address to the RIRB
+  EFI_PHYSICAL_ADDRESS RirbPhysAddress;
+  /// Pointer to the CORB in virtual memory
+  HDA_CORB_ENTRY* Corb;
+  /// Pointer to the RIRB in virtual memory
+  HDA_RIRB_RESPONSE* Rirb;
+  /// Current CORB write pointer
+  UINT8 CorbWritePtr;
+  /// Current RIRB read pointer
+  UINT8 RirbReadPtr;
+  /// List of node IDs
+  UINT8 Nodes[15];
+} HDA_CONTROLLER;
+
+// HDA vendor ID/device list
+// Necessary to find an HDA controller, apparently
+// Taken from http://wiki.kolibrios.org/wiki/Intel_High_Definition_Audio
+// {vendor ID, device ID}
+static UINT16 HDA_DEVICES[][] = {
+  {0x8086, 0x2668},
+  {0x8086, 0x269A},
+  {0x8086, 0x27D8},
+  {0x8086, 0x284B},
+  {0x8086, 0x2911},
+  {0x8086, 0x293E},
+  {0x8086, 0x293F},
+  {0x8086, 0x3A3E},
+  {0x8086, 0x3A6E},
+  {0x8086, 0x3B56},
+  {0x8086, 0x3B57},
+  {0x8086, 0x811B},
+  {0x8086, 0x1C20},
+  {0x10DE, 0x026C},
+  {0x10DE, 0x0371},
+  {0x10DE, 0x03E4},
+  {0x10DE, 0x03F0},
+  {0x10DE, 0x044A},
+  {0x10DE, 0x044B},
+  {0x10DE, 0x055C},
+  {0x10DE, 0x055D},
+  {0x10DE, 0x0774},
+  {0x10DE, 0x0775},
+  {0x10DE, 0x0776},
+  {0x10DE, 0x0777},
+  {0x10DE, 0x07FC},
+  {0x10DE, 0x07FD},
+  {0x10DE, 0x0AC0},
+  {0x10DE, 0x0AC1},
+  {0x10DE, 0x0AC2},
+  {0x10DE, 0x0AC3},
+  {0x10DE, 0x0D94},
+  {0x10DE, 0x0D95},
+  {0x10DE, 0x0D96},
+  {0x10DE, 0x0D97},
+  {0x1002, 0x437B},
+  {0x1002, 0x4383},
+  {0x1106, 0x3288},
+  {0x1039, 0x7502},
+  {0x10B9, 0x5461},
+  {0x6549, 0x1200},
+  {0x17F3, 0x3010}
+};
 
